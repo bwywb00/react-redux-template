7ebb3885f4466ac910d5f1688567aba1fc4d1cac
Sung <bwywb00@gmail.com>
2018-12-25 12:50:00 +0900

    GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    user-select: none;
+  }
+`;
+
+export default GlobalStyle;
--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,11 @@
 import React from "react";
 import App from "components/App";
+import GlobalStyle from "./GlobalStyle";
 
 const Root = () => {
   return (
     <div>
+      <GlobalStyle />
       <App />
     </div>
   );
